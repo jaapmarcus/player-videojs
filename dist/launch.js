3e2558795b78cb059acd83bdbce97beb54488e27
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2024-10-08 13:16:37 +0200

    Add anchor check

--- dist/launch.js
+++ dist/launch.js
@@ -1,3 +1,61 @@
 window.onload = function () {
-    console.log('launch.js loaded');   
+    fireWhenReady();
+};
+
+var attemps = 0;
+function fireWhenReady() {
+    if (typeof __cmp != "undefined") {
+      __cmp("addEventListener", ["consent", loadAdsight, false], null);
+      let result = __cmp("getCMPData");
+      if (result) {
+        loadAdsight();
+      }
+    } else {
+      if (typeof __tcfapi != "undefined") {
+        __tcfapi("addEventListener", 2, loadAdsight_tcf);
+      } else setTimeout(fireWhenReady, 100);
+    }
+    if (attemps > 20) {
+      return;
+    }
+    attemps++;
 }
+
+
+function loadAdsight() {
+    if (success && tcData.eventStatus === "useractioncomplete") {
+        tfcData = tcData;
+        tcConsentString = tcData.tcString;
+        
+      }
+      if (success && tcData.eventStatus === "tcloaded") {
+        tfcData = tcData;
+        tcConsentString = tcData.tcString;
+        
+      }
+}
+
+function loadAdsight_tcf() {
+    if (success && tcData.eventStatus === "useractioncomplete") {
+        tfcData = tcData;
+        tcConsentString = tcData.tcString;
+        
+      }
+      if (success && tcData.eventStatus === "tcloaded") {
+        tfcData = tcData;
+        tcConsentString = tcData.tcString;
+        
+      }
+    };
+    
+    function loadAdsight() {
+      if (typeof MediaElementPlayer == "undefined") {
+        setTimeout(firePlayer, 100);
+      } else {
+
+      }
+}
+
+function firePlayer() {
+    
+  }
