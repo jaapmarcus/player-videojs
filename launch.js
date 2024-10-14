de524acceb8c2734373d16f7e00b2f32e1f6b015
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2024-10-14 15:17:48 +0200

    remove player form html

--- launch.js
+++ launch.js
@@ -1,4 +1,20 @@
 window.onload = function () {
+  console.log(adsight_config);
+  //check if adsight_config is defined
+  if (!document.getElementById('adsight-anchor')) {
+      return;
+  }
+  parentNode = document.getElementById('adsight-anchor').parentNode;
+  para = parentNode.getElementsByTagName('p');
+  para = Array.from(para);
+  console.log(para[adsight_config.paragraphs]);
+  div = document.createElement('div');
+  div.id = 'adsight-video-container';
+  parentNode.insertBefore(div, para[adsight_config.paragraphs]);
+  div.innerHTML = `<div id="adsight-video-parent"><video id="player" controls class="video-js" muted="muted" playsinline="playsinline" preload="metadate" fluid="true">
+    Your browser does not support the video tag.
+    </video></div><div id="adsight-video-title" class="adsight-slider-header-not-active" style="display:none"><div id="adsight-close-button"><a href="" id="adsight-close-link-mobile" height="16px" width="16px"><img src="https://cdn.adsight.nl/close-darker.svg" style="max-height:16px !important" /></a></div></div>`
+
   pbjs.que.push(function () {
     console.log('Prebid loaded');
     pbjs.setConfig({
@@ -76,26 +92,3 @@ console.log('Prebid loaded');
 
 var pbjs = pbjs || {};
 pbjs.que = pbjs.que || [];
-
-
-
-function loadAnchor() {
-    /*console.log(adsight_config);
-    //check if adsight_config is defined
-    if (!document.getElementById('adsight-anchor')) {
-        return;
-    }
-    parentNode = document.getElementById('adsight-anchor').parentNode;
-    para = parentNode.getElementsByTagName('p');
-    para = Array.from(para);
-    console.log(para[adsight_config.paragraphs]);
-    div = document.createElement('div');
-    div.id = 'adsight-video-container';
-    parentNode.insertBefore(div, para[adsight_config.paragraphs]);
-    div.innerHTML = `<div id="adsight-video-parent"><video id="player" controls class="video-js" muted="muted" playsinline="playsinline" preload="metadate" fluid="true">
-      Your browser does not support the video tag.
-      </video></div><div id="adsight-video-title" class="adsight-slider-header-not-active" style="display:none"><div id="adsight-close-button"><a href="" id="adsight-close-link-mobile" height="16px" width="16px"><img src="https://cdn.adsight.nl/close-darker.svg" style="max-height:16px !important" /></a></div></div>`
-  preloadPreBid();*/
-  
-
-}
